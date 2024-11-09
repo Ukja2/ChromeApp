699a02c828a54cbef2d12d877816e06e09f7ead9
Ukja2 <jaewooklee739@gmail.com>
2024-11-09 14:22:48 +0900

    로그인 유지

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 const loginForm = document.querySelector("#login-form")
-const loginInput = document.querySelector(" #login-form input");
-const greeting = document.querySelector(" #greeting")
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
-const HIDDEN_CLASSNAME = "hidden"; //문자열과 같이 중요하지 않은 정보를 담을 때는 대문자로 지정
+//문자열과 같이 중요하지 않은 정보를 담을 때는 대문자로 지정
+const HIDDEN_CLASSNAME = "hidden"; 
+const USERNAME_KEY = "username";
 
-function onLoginSubmit(event){ //onLoginSubmit 함수가 호출될 때, 브라우저는 발생한 이벤트에 대한 정보를 담은 event 객체를 자동으로 만들어 함수의 첫 번째 매개변수로 전달함 
+//onLoginSubmit 함수가 호출될 때, 브라우저는 발생한 이벤트에 대한 정보를 담은 event 객체를 자동으로 만들어 함수의 첫 번째 매개변수로 전달함 
+function onLoginSubmit(event){ 
     
     //html의 form은 submit을 하면 브라우저의 기본 동작인 새로고침을 하는데 이를 막아주는 함수 이를 통해 submit을 할때마다 새로고침 되는 현상을 예방할 수 있음
     //event 함수는 preventDefault 함수를 기본적으로 갖고 있음
     event.preventDefault(); 
 
-        //로그인 시 loginForm 숨김고 h1을 나타나게
+    //로그인 시 loginForm 숨기고 h1을 나타나게
     loginForm.classList.add(HIDDEN_CLASSNAME); //loginForm에 css의 hidden 클래스 추가
     const username = loginInput.value; //입력값을 username에 저장
 
-    localStorage.setItem("username", username)//localStorage.setItem: 로컬저장소에 해당 key와 value를 저장함
+    //localStorage.setItem: 로컬저장소에 해당 key와 value를 저장함
     //username에 Ukja2를 넣으면 Storage에 key는 username value는 Ukja2와 같은 형태로 저장됨
+    localStorage.setItem(USERNAME_KEY, username);
     
-    greeting.innerText = `Hello ${username}` //텍스트를 "Hello" + username 형식으로 변환, * 백틱 기호를 사용해야만 이와 같은 형태로 작성가능
-    greeting.classList.remove(HIDDEN_CLASSNAME); //숨겨지게 만드는 hidden 클래스를 삭제 -> 화면에 나타나게 됨
+    
+    paintGreeting(username);
+    }
+
+//로그인 후 사용자 정보 표현 함수
+function paintGreeting(username){ 
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
 
-
-//아래에서 onLoginSubmit()과 같이 괄호를 치면 자동적으로 실행 후 이후의 이벤트에 대해서는 아무런 동작을 하지 않게된다.
-//submit이라는 이벤트가 발생할 때만 onLoginSubmit 함수가 실행되는 것이 목표이기 때문
-//아래의 코드는 form이 제출될 때마다 onLoginSubmit함수 호출
-loginForm.addEventListener("submit", onLoginSubmit);
- 
+const savedUsername = localStorage.getItem(USERNAME_KEY); //username 키에 저장된 값을 불러옴
+if(savedUsername === null){ 
+    loginForm.classList.remove(HIDDEN_CLASSNAME); //첫 페이지에서 SavedUsename에 USERNNAME-KEY의 값이 null이면 hidden 클래스 삭제 
+    loginForm.addEventListener("submit" , onLoginSubmit);
+} else {
+    paintGreeting(savedUsername)
+}
