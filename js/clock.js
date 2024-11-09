3c2ed50d4c1a7f098e020dd852ce5b2dc608ccc4
Ukja2 <jaewooklee739@gmail.com>
2024-11-09 15:11:57 +0900

    시계 padStart 옵션 추가

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,13 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 
+    //getxxxx()는 숫자형태이기 때문에 padstart와 함께 사용할 수 없음 그래서 String()을 사용해 문자열로 변환 후 padstart를 사용해야 한다.
+    //padStart()를 통해 1(시,분,초)일때 01의 형태로 나타낼 수 있다.
+    const hours =String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 //함수를 호출함으로써 사아트가 load되자마자 getClock()을 실행하고 이후 매초마다 실행되도록
