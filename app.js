44b0f97582b3152f44d9a2d062982a8f4c82ca4d
Ukja2 <jaewooklee739@gmail.com>
2024-11-09 13:04:17 +0900

    입력받은 값을 storage에 저장하기

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ function onLoginSubmit(event){ //onLoginSubmit 함수가 호출될 때, 브라�
     //html의 form은 submit을 하면 브라우저의 기본 동작인 새로고침을 하는데 이를 막아주는 함수 이를 통해 submit을 할때마다 새로고침 되는 현상을 예방할 수 있음
     //event 함수는 preventDefault 함수를 기본적으로 갖고 있음
     event.preventDefault(); 
-    
-    //로그인 시 loginForm 숨김고 h1을 나타나게
+
+        //로그인 시 loginForm 숨김고 h1을 나타나게
+    loginForm.classList.add(HIDDEN_CLASSNAME); //loginForm에 css의 hidden 클래스 추가
     const username = loginInput.value; //입력값을 username에 저장
-    loginForm.classList.add(HIDDEN_CLASSNAME);  //loginForm에 css의 hidden 클래스 추가
+
+    localStorage.setItem("username", username)//localStorage.setItem: 로컬저장소에 해당 key와 value를 저장함
+    //username에 Ukja2를 넣으면 Storage에 key는 username value는 Ukja2와 같은 형태로 저장됨
+    
     greeting.innerText = `Hello ${username}` //텍스트를 "Hello" + username 형식으로 변환, * 백틱 기호를 사용해야만 이와 같은 형태로 작성가능
     greeting.classList.remove(HIDDEN_CLASSNAME); //숨겨지게 만드는 hidden 클래스를 삭제 -> 화면에 나타나게 됨
 }
 
-function handleLinkClick(){
-    alert("clicked!");
-}
+
 
 
 //아래에서 onLoginSubmit()과 같이 괄호를 치면 자동적으로 실행 후 이후의 이벤트에 대해서는 아무런 동작을 하지 않게된다.
