9390937515859e6305e59d0887ef3942e42d1268
Ukja2 <jaewooklee739@gmail.com>
2024-11-10 18:42:46 +0900

    주석 수정

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ function getClock(){
 
     //getxxxx()는 숫자형태이기 때문에 padstart와 함께 사용할 수 없음 그래서 String()을 사용해 문자열로 변환 후 padstart를 사용해야 한다.
     //padStart()를 통해 1(시,분,초)일때 01의 형태로 나타낼 수 있다.
-    const hours =String(date.getHours()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}:${seconds}`
--- js/greeting.js
+++ js/greeting.js
@@ -22,13 +22,13 @@ function onLoginSubmit(event){
     localStorage.setItem(USERNAME_KEY, username);
     
     paintGreeting(username);
-    toDoForm.classList.remove(HIDDEN_CLASSNAME)
+    toDoForm.classList.remove(HIDDEN_CLASSNAME)//로그인을 하면 투두리스트 입력창 생성
     }
 
 //로그인 후 사용자 정보 표현 함수
 function paintGreeting(username){ 
     greeting.innerText = `안녕하세요 ${username}님!`; //비어있는 h1에 텍스트 추가
-    greeting.classList.remove(HIDDEN_CLASSNAME); //로그인을 하면 투두리스트 입력창 생성
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
 }
 
 
